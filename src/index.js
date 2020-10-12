60bdb98e6c784f3893c3137c0a1a67426d86517d
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-10-12 10:10:35 -0500

    12 10 2020

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-
+const exhbs = require("express-handlebars");
+const path = require("path");
 //Configuraciones
 app.set("port", process.env.PORT || 3500);
+app.set("views", path.join(__dirname,"views"))
+app.engine(".hbs", exhbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"),"layouts"),
+    partialsDir: path.join(app.get("views"),"partials"),
+    extname: ".hbs",
+    helpers: require("./lib/handlebars")
+}))
+app.set("view engine", ".hbs");
 //Midleware
 app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 //Global
-
+app.use((req, res, next) =>{
+next();
+});
 //Rutas
 app.use(require("./routes/"));
+app.use(require("./routes/auth"));
+app.use("/links", require("./routes/"));
 //Public
-
+app.use(express.static(path.join(__dirname, "public")))
 //Servidor
 app.listen(app.get("port"),()=>{
 console.log("server en ", app.get("port"));

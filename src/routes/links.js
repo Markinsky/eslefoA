ac08b8d82d80eaf3d80030d464111f9e5840820a
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-10-15 13:19:12 -0500

    15 10 2020 2

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,23 +7,21 @@ router.get("/add", (req, res) =>{
     res.render("links/add");
 });
 
-router.post("/add", async(req, res) =>{
+router.post("/add", (req, res) =>{
     const {nombre, apPat, apMat, nacimiento, contraA, contraB, email, numero} = req.body;
-    const newUsser = {
-        nombre,
-        apPat,
-        apMat,
-        nacimiento,
-        contraA,
-        contraB,
-        email,
-        numero
-    };
-    await pool.query("INSERT INTO aspirante set ?",[newUsser]);
+    const hola = pool.query("INSERT INTO aspirante (nombre, apPat, apMat, birthday, celular, correo) values ($1, $2, $3, $4, $5, $6)",[nombre,apPat,apMat,nacimiento,numero,email]);
+    console.log("CONSOLA: ",hola);
     res.send("Recibido uwu");
 });
 
 router.get("/register", (req, res) =>{
     res.render("links/register");
+});
+
+router.get("/faq", (req, res) =>{
+    res.render("links/faq");
+});
+router.get("/login", (req, res) =>{
+    res.render("links/login");
 });
 module.exports = router;

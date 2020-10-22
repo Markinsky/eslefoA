1a6c0d1f9d00f1e91c2b392dba8d7b4fab19c8f3
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-10-22 09:56:46 -0500

    22 10 2020

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,50 +7,55 @@ router.get("/add", (req, res) =>{
 });
 
 router.post("/add", async (req, res) =>{
-    try{
-    const nombre = req.body.nombre;
-    const apPat = req.body.apPat;
-    const apMat = req.body.apMat;
-    const nacimiento = req.body.nacimiento;
-    const numero = req.body.numero;
-    const email = req.body.email;
-    const passA = req.body.contraA;
-    const passB = req.body.contraB;
-    var numeroLenght = numero.length;
-    if(passA != passB){
-        console.log("Contraseña erronea");
-    }else{
-        if(numeroLenght==10){
-            const yy = [
-                nombre,
-                apPat,
-                apMat,
-                nacimiento,
-                numero,
-                email,
-            ];
-            const ww =[
-                email,
-                passA,
-                id,
-            ];
-            //const qq = "INSERT INTO aspirante(nombre,appat,apmat,birthday,numero,correo) values ($1, $2, $3, $4, $5, $6)";
-            //const qq = "INSERT INTO login(usser, pass,id,funcion) values ($1, $2, $3,'aspirante')";
-            //const efe = await pool.query(qq, yy);
-        }else{
-           console.log("Error")
-            
-        }
-    }
-    }catch(e){
-        console.log("catch:", e)
-    }
+  
 });
 
 router.get("/register", (req, res) =>{
     res.render("links/register");
 });
 
+router.post("/register",(req, res)=>{
+    try{
+        const nombre = req.body.nombre;
+        const apPat = req.body.apPat;
+        const apMat = req.body.apMat;
+        const nacimiento = req.body.nacimiento;
+        const numero = req.body.numero;
+        const email = req.body.email;
+        const passA = req.body.contraA;
+        const passB = req.body.contraB;
+        var numeroLenght = numero.length;
+        const id = lastID();
+        const codigo = newCode();
+        if(passA != passB){
+            console.log("Contraseña erronea");
+        }else{
+            if(numeroLenght==10){
+                const yy = [
+                    nombre,
+                    apPat,
+                    apMat,
+                    nacimiento,
+                    numero,
+                    email,
+                ];
+                const ww =[
+                    email,
+                    passA,
+                    id,
+                ];
+               // const qq = "INSERT INTO aspirante(nombre,appat,apmat,birthday,numero,correo) values ($1, $2, $3, $4, $5, $6)";
+                //const pp = "INSERT INTO login(usser, pass,id,funcion) values ($1, $2, $3,'aspirante')";
+                //const efe = await pool.query(qq, yy);
+            }else{
+               console.log("Error")
+                
+            }
+        }
+        }catch(e){
+            console.log("catch:", e)
+        }
+});
 router.get("/faq", (req, res) =>{
     res.render("links/faq");
 });
@@ -59,13 +64,23 @@ router.get("/login", (req, res) =>{
 });
 
 const lastID = async(req, res) =>{
-    const queryGet = 'SELECT id_aspirante from aspirante ORDER BY id_aspirante DESC LIMIT 1;';
     try{
-        const { rows } = await dbQuery.query(queryGet);
-        const id = rows;
+        const queryGet = await pool.query('SELECT id_aspirante from aspirante ORDER BY id_aspirante DESC LIMIT 1;');
+        const id = queryGet.rows;
+        return id;
     }catch(e){
-
+        console.log("error lastId", e)
     }
 };
 
+const newCode = async(req,res) =>{
+    try{
+        const getCode = await pool.query('SELECT codigo from aspirante ORDER BY id_aspirante DESC LIMIT 1');
+        const code = getCode.rows;
+        console.log("codigo" , getCode);
+        return code +1;
+    }catch(e){
+        console.log("error newCode", e)
+    }
+};
 module.exports = router;

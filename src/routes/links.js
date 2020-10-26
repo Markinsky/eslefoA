c5c7735efdbae3ee0425b819b0ea82cecedc115d
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-10-26 10:04:32 -0600

    26 10 2020

--- src/routes/links.js
+++ src/routes/links.js
@@ -15,49 +15,7 @@ router.get("/register", (req, res) =>{
 });
 
 router.post("/register", async(req, res)=>{
-    try{
-        const nombre = req.body.nombre;
-        const apPat = req.body.apPat;
-        const apMat = req.body.apMat;
-        const nacimiento = req.body.nacimiento;
-        const numero = req.body.numero;
-        const email = req.body.email;
-        const passA = req.body.contraA;
-        const passB = req.body.contraB;
-        var numeroLenght = numero.length;
-        const codigo = await newCode();
-        var verE = await verEmail(email);
-        if(passA != passB || verE === false){
-            console.log("Contraseña erronea o correo repetido");
-        }else{
-            if(numeroLenght==10){
-                const yy = [
-                    nombre,
-                    apPat,
-                    apMat,
-                    nacimiento,
-                    numero,
-                    email,
-                    codigo
-                ];
-               const qq = "INSERT INTO aspirante(nombre,appat,apmat,birthday,numero,correo, codigo) values ($1, $2, $3, $4, $5, $6, $7)";
-            const pp = "INSERT INTO login(usser, pass,id,funcion) values ($1, $2, $3,'aspirante')";
-                const efeA = await pool.query(qq, yy);
-                const id = await lastID();
-                const ww =[
-                    codigo,
-                    passA,
-                    id,
-                ];
-                const efeB = await pool.query(pp, ww);
-            }else{
-               console.log("Error")
-                
-            }
-        }
-        }catch(e){
-            console.log("catch:", e)
-        }
+  
 });
 router.get("/faq", (req, res) =>{
     res.render("links/faq");
@@ -66,44 +24,4 @@ router.get("/login", (req, res) =>{
     res.render("links/login");
 });
 
-const lastID = async(req, res) =>{
-    try{
-        const queryGet = await pool.query('SELECT id_aspirante from aspirante ORDER BY id_aspirante DESC LIMIT 1;');
-        const id = queryGet.rows[0].id_aspirante;
-        return id;
-    }catch(e){
-        console.log("error lastId", e)
-    }
-};
-
-const newCode = async(req,res) =>{
-    try{
-        const getCode = await pool.query('SELECT codigo from aspirante ORDER BY id_aspirante DESC LIMIT 1');
-        var count = getCode.rows[0].codigo;
-        var sum = parseInt(count);
-        var code = sum + 1;
-        return code;
-    }catch(e){
-        console.log("error newCode", e)
-    }
-};
-
-var verEmail = async(email) =>{
-    try{
-        const query = ('SELECT count(*) from aspirante where correo = $1;');
-        const correo = [email];
-        const qq = await pool.query(query, correo);
-        var count = qq.rows[0].count;
-        var res = parseInt(count);
-        if(res != 0){
-            //console.log("Mas de uno" , res);
-            return false;
-        }else{
-            //console.log("Resultado:" , res);
-            return true;
-        }   
-    }catch(e){
-        console.log("error en el verEmail", e)
-    }
-};
 module.exports = router;

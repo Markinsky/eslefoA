08f6ced3e45d0dd781a76ac11a65752e8711d19d
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-11-10 09:57:40 -0600

    10 11 2020

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,29 +2,30 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
-router.get("/add", (req, res) =>{
-    res.render("links/add");
+router.get("/add", (req, res) => {
+  res.render("links/add");
 });
 
-router.post("/add", async (req, res) =>{
-  
+router.post("/add", async (req, res) => {});
+
+router.get("/register", (req, res) => {
+  res.render("links/register");
 });
 
-router.get("/register", (req, res) =>{
-    res.render("links/register");
+router.post("/register", async (req, res) => {});
+
+router.get("/faq", (req, res) => {
+  res.render("links/faq");
+});
+router.get("/login", (req, res) => {
+  res.render("links/login");
 });
 
-router.post("/register", async(req, res)=>{
-  
-});
-router.get("/faq", (req, res) =>{
-    res.render("links/faq");
-});
-router.get("/login", (req, res) =>{
-    res.render("links/login");
+router.get("/aboutus", (req, res) => {
+  res.render("links/aboutus");
 });
 
-router.get("/aboutus", (req, res) =>{
-    res.render("links/aboutus");
+router.get("/rules", (req, res) => {
+  res.render("links/rules");
 });
 module.exports = router;

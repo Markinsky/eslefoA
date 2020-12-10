5dd89e0ec9ba43d08cacd6a579f474baeabcc3ed
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-12-10 14:44:14 -0600

    10 12 2020 2

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const { isLoggedIn } = require("../lib/auth");
+const pool = require("../db");
 
 router.get("/signup", (req, res) => {
   res.render("auth/register");
@@ -30,6 +31,44 @@ router.post("/signin", (req, res, next) => {
 
 router.get("/profile", isLoggedIn, (req, res) => {
   res.render("profile");
+});
+
+router.get("/editdata/:id_aspirante", async (req, res) => {
+  const { id_aspirante } = req.params;
+  const qq = await pool.query(
+    "SELECT * FROM aspirante WHERE id_aspirante = $1 AND funcion = 'aspirante'",
+    [id_aspirante]
+  );
+  const arr = qq.rows[0];
+  res.render("auth/edituser", { arr });
+});
+
+router.post("/editdata/:id_aspirante", async (req, res) => {
+  try {
+    const { id_aspirante } = req.params;
+    const { nombre, apPat, apMat, date, numero } = req.body;
+    const length = numero.length;
+    if (length == 10) {
+      const juju = await pool.query(
+        "UPDATE aspirante SET nombre =$1, appat = $2, apmat = $3, numero =$4, birthday=$5 WHERE id_aspirante = $6",
+        [nombre, apPat, apMat, numero, date, id_aspirante]
+      );
+      const back = juju.rowCount;
+      if (back == 1) {
+        req.logOut();
+        req.flash("success", "Datos editados, favor de hacer login de nuevo");
+        res.redirect("/signin");
+      } else {
+        req.flash("error", "Ha ocurrido un error");
+        res.redirect("/profile");
+      }
+    } else {
+      req.flash("error", "Datos invalidos");
+      res.redirect("/profile");
+    }
+  } catch (e) {
+    console.log("Error edit jaja", e);
+  }
 });
 
 router.get("/logout", (req, res) => {
